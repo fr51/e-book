fcd0b64fcdec9931376c48f13b68e951c6899528
François-Emile <francoisemile5@orange.fr>
2018-01-24 16:29:22 +0100

    commit

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,9 +8,6 @@ use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/", name="homepage")
-     */
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
@@ -18,4 +15,31 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+
+    public function accueilAction (Request $request)
+	{
+		$em=$this->getDoctrine ()->getManager ();
+
+		$livres=$em->getRepository ("AppBundle:livre")->findAll ();
+
+		return ($this->render ("@App/accueil.html.twig", ["livres" => $livres]));
+	}
+
+	public function detailsAction (Request $request, $id)
+	{
+		$em=$this->getDoctrine ()->getManager ();
+
+		$livre=$em->getRepository ("AppBundle:livre")->find ($id);
+
+		return ($this->render ("@App/details.html.twig", ["livre" => $livre]));
+	}
+
+	public function commandeAction (Request $request, $id)
+	{
+		$em=$this->getDoctrine ()->getManager ();
+
+		$livre=$em->getRepository ("AppBundle:livre")->find ($id);
+
+		return ($this->render ("@App/commande.html.twig", ["livre" => $livre]));
+	}
 }

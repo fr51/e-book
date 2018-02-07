787eb975f9830110faed2180fb91d7cdd53b697e
Marie <marie.cuer@hotmail.fr>
2018-02-07 17:27:13 +0100

    Login redirection  , Css

---
+++ app/Resources/FOSUserBundle/Controller/SecurityController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace AppBundle\Controller;
+use FOS\UserBundle\Controller\SecurityController as BaseController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use AppBundle\Entity\utilisateur;
+use Symfony\Component\Security\Core\Security;
+
+class SecurityController extends BaseController
+{
+    public function loginAction()
+    {
+        
+        /** @var $session Session */
+        $session = $request->getSession();
+        
+                $authErrorKey = Security::AUTHENTICATION_ERROR;
+                $lastUsernameKey = Security::LAST_USERNAME;
+        
+                // get the error if any (works with forward and redirect -- see below)
+                if ($request->attributes->has($authErrorKey)) {
+                    $error = $request->attributes->get($authErrorKey);
+                } elseif (null !== $session && $session->has($authErrorKey)) {
+                    $error = $session->get($authErrorKey);
+                    $session->remove($authErrorKey);
+                } else {
+                    $error = null;
+                }
+        
+                if (!$error instanceof AuthenticationException) {
+                    $error = null; // The value does not come from the security component.
+                }
+        
+                // last username entered by the user
+                $lastUsername = (null === $session) ? '' : $session->get($lastUsernameKey);
+        
+                $csrfToken = $this->has('security.csrf.token_manager')
+                    ? $this->get('security.csrf.token_manager')->getToken('authenticate')->getValue()
+                    : null;
+        
+                return $this->renderLogin(array(
+                    'last_username' => $lastUsername,
+                    'error' => $error,
+                    'csrf_token' => $csrfToken,
+                ));
+    }  
+}

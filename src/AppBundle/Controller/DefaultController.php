f10e2f83bdec764fbbb88f0f15a02b8d70578ea9
François-Emile <francoisemile5@orange.fr>
2018-02-28 11:04:05 +0100

    commande

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,13 +2,17 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\commande;
+use AppBundle\Entity\details_commande;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use AppBundle\Entity\utilisateur;
 use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Validator\Constraints\Date;
 
 class DefaultController extends Controller
 {
@@ -62,12 +66,41 @@ class DefaultController extends Controller
 		return ($this->render ("@App/details.html.twig", ["livre" => $livre]));
 	}
 
-	public function commandeAction (Request $request, $id)
+	public function commandeAction (Request $requete)
 	{
+		$id=(int) $requete->request->get ("livre");
+		$quantite=$requete->request->get ("quantite");
+
+		$utilisateur=$this->container->get ("security.token_storage")->getToken ()->getUser ();
+
 		$em=$this->getDoctrine ()->getManager ();
 
 		$livre=$em->getRepository ("AppBundle:livre")->find ($id);
 
-		return ($this->render ("@App/commande.html.twig", ["livre" => $livre]));
+		$prix_unitaire=$livre->getPrixUnitaire ();
+		$prix=$prix_unitaire*$quantite;
+
+		$commande=new commande ();
+
+		$commande->setUtilisateur ($utilisateur);
+		$commande->setDateCommande (new \DateTime ());
+		$commande->setDateEnvoi (new \DateTime ());
+
+		$em->persist ($commande);
+		$em->flush ();
+
+		$details_commande=new details_commande ();
+		$details_commande->setCommande ($commande);
+		$details_commande->setLivre ($livre);
+		$details_commande->setPrix ($prix);
+		$details_commande->setQuantite ($quantite);
+
+		$em->persist ($details_commande);
+		$em->flush ();
+
+		$reponse=new Response ();
+		$reponse->setStatusCode (200);
+
+		return ($reponse);
 	}
 }
--- src/AppBundle/Entity/utilisateur.php
+++ src/AppBundle/Entity/utilisateur.php
@@ -13,120 +13,20 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class utilisateur extends BaseUser
 {
-    /**
-     * @var int
-     *
-     * @ORM\Column(name="id", type="integer")
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
-    protected $id;
-    /**
-     * @var \commande
-     *
-     * @ORM\OneToMany(targetEntity="commande" , mappedBy="utilisateur" , cascade={"remove","persist"})
-     */
-    private $utilisateurs;
-    /**
-     * Get id
-     *
-     * @return int
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
+	/**
+	 * @ORM\Id
+	 * @ORM\Column(type="integer")
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 */
+	protected $id;
 
-    /**
-     * Set nomUtilisateur
-     *
-     * @param string $nomUtilisateur
-     *
-     * @return utilisateur
-     */
-    public function setNomUtilisateur($nomUtilisateur)
-    {
-        $this->nomUtilisateur = $nomUtilisateur;
-
-        return $this;
-    }
-
-    /**
-     * Get nomUtilisateur
-     *
-     * @return string
-     */
-    public function getNomUtilisateur()
-    {
-        return $this->nomUtilisateur;
-    }
-
-    /**
-     * Set prenomUtilisateur
-     *
-     * @param string $prenomUtilisateur
-     *
-     * @return utilisateur
-     */
-    public function setPrenomUtilisateur($prenomUtilisateur)
-    {
-        $this->prenomUtilisateur = $prenomUtilisateur;
-
-        return $this;
-    }
-
-    /**
-     * Get prenomUtilisateur
-     *
-     * @return string
-     */
-    public function getPrenomUtilisateur()
-    {
-        return $this->prenomUtilisateur;
-    }
-
-    
-    /**
-     * Constructor
-     */
-    public function __construct()
-    {
-        parent::__construct();
-        // your own logic
-        $this->utilisateurs = new \Doctrine\Common\Collections\ArrayCollection();
-        
-    }
-    /**
-     * Add utilisateur
-     *
-     * @param \AppBundle\Entity\commande $utilisateur
-     *
-     * @return utilisateur
-     */
-    public function addUtilisateur(\AppBundle\Entity\commande $utilisateur)
-    {
-        $this->utilisateurs[] = $utilisateur;
-
-        return $this;
-    }
-
-    /**
-     * Remove utilisateur
-     *
-     * @param \AppBundle\Entity\commande $utilisateur
-     */
-    public function removeUtilisateur(\AppBundle\Entity\commande $utilisateur)
-    {
-        $this->utilisateurs->removeElement($utilisateur);
-    }
-
-    /**
-     * Get utilisateurs
-     *
-     * @return \Doctrine\Common\Collections\Collection
-     */
-    public function getUtilisateurs()
-    {
-        return $this->utilisateurs;
-    }
+	/**
+	 * Get id
+	 *
+	 * @return int
+	 */
+	public function getId()
+	{
+		return $this->id;
+	}
 }
